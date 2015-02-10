865c7f25f4615bc8729c991d5fc7568bb4d0ab99
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-02-10 17:30:48 +0200

    no need to export to root browserify handles it for us

--- src/wrap_footer.js
+++ src/wrap_footer.js
@@ -1,9 +1,5 @@
 }).call(moduleScope);
 
-if (typeof exports !== 'undefined') {
-	module.exports = moduleScope.Module;
-} else {
-	root.enetModule = moduleScope.Module;
-}
+module.exports = moduleScope.Module;
 
 }).call(this);
--- src/wrap_header.js
+++ src/wrap_header.js
@@ -1,7 +1,6 @@
 ;
 (function () {
 		"use strict";
-		var root = this;
 		var moduleScope = {};
 
 		(function () {
