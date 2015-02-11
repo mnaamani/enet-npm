5853a2a11616bcc47e5b2057e67c2afd28a89e3c
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-02-11 11:50:01 +0200

    refactoring - breakout bindings.js to seprate modules

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-module.exports = require("./lib/bindings.js");
+var ENETModule = require("./build/enet.js");
+
+var jsapi_ = ENETModule.jsapi;
+
+var enet = module.exports = {};
+
+enet.Host = require("./lib/Host.js").Host;
+enet.createServer = require("./lib/Host.js").createServer;
+enet.createClient = require("./lib/Host.js").createClient;
+enet.createServerFromSocket = require("./lib/Host.js").createServerFromSocket;
+enet.Event = require("./lib/Event.js").Event;
+enet.Address = require("./lib/Address.js").Address;
+enet.Packet = require("./lib/Packet.js").Packet;
+enet.Peer = require("./lib/Peer.js").Peer;
+enet.Buffer = require("buffer").Buffer; //for use in chrome app when creating packets
+enet.PACKET_FLAG = require("./lib/PACKET_FLAG.js").PACKET_FLAG;
+enet.PEER_STATE = require("./lib/PEER_STATE.js").PEER_STATE;
+
+enet.init = function (func) {
+	var funcPointer = ENETModule["Runtime_addFunction"](function (host_ptr) {
+		var addr = new enet.Address(jsapi_.host_get_receivedAddress(host_ptr));
+		return func(addr.address(), addr.port());
+	});
+	jsapi_.init(funcPointer);
+};
+
+process.removeAllListeners("uncaughtException"); //emscripten catches and throws again!
+process.on("uncaughtException", function (e) {
+	//catch uncaught exceptions
+	// node's .bind() on dgram sockets throws an async exception
+	// it will be caught in socket.on("error") in createHost();
+	// but we catch the exception here to prevent app crashing
+	console.error("uncaught exception:", e);
+});
